fa69647ceb61c65210796e2e92f1cf68a5b45dd3
Adrian Leal <64176115+lea19019@users.noreply.github.com>
2021-05-21 10:32:24 -0500

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,70 +1,132 @@
-fun main(args: Array<String>) {
-    // Comments in Kotlin
-    // To declare a variable just start with var, no int or char or data type is needed
-    var age = 23
-    age = 30  // You can also reassign variable with typing the name and writing the equal sign
+// Imports that will help us to read text files
+import java.io.File
+import java.io.InputStream
 
-    // To declare an immutable variable use val
-    val anotherAge = 25
+// Class created to hold our Alebrije information
+class Alebrije {
+    // Variables containing strings, integers and booleans
+    var name: String? = ""
+    var age: Int = 0
+    var home : String? = ""
+    var isGood: Boolean = true
 
-    // You can also specify the data-type
-    val favCandy: String = "Snicker"
+    // These are types of collections in Kotlin, lists
+    var hobbies = mutableListOf<String?>("","")
+    var habilities = mutableListOf<String?>("","")
 
-    // This is interpolation in Kotlin
-    println("I am $age years old, I fear to reach $anotherAge and my favorite candy is $favCandy")
+}
 
-    var name = "Vicente"
-    /*
-    This is how to make multiline comments
-     */
+// Function created to request the Alebrije data from user
+fun getAlebrijeData(alebrije: Alebrije){
 
-    var radius = 6
-    var pi = 3.14
-    var c = radius * pi * 2 // Kotlin is smart enough to make the conversion from different types of numbers
-    var c2: Int = radius * pi.toInt() * 2 // You can also change the data type
+    print("What is the name of your alebrije: ")
+    alebrije.name = readLine() // readLine() will get the input from the user
 
-    // Both print and println will output the text, but println will print the next line
-    print(c)
-    print('\n') // This is how you print a new line with text
-    println(c2)
+    print("How old is you alebrije: ")
+    alebrije.age = Integer.valueOf(readLine())
 
-    // You can also do the + - equals
-    var wallet = 45
-    wallet -= 5
-    println(wallet)
-    wallet += 15
-    println(wallet)
+    print("Where does you alebrije lives? ")
+    alebrije.home = readLine()
 
-    var myAge = 23
-    myAge += 40
-    /*
-    I hopefully be retired in 40 years
-     */
+    var answer: String?
+    // Representation of a while loop in Kotlin
+    do {
+        print("Is he good? (Y/N) ")
+        answer = readLine()
 
-    var isTheDogAlive = true
+        if (answer == "N" || answer == "n")
+            alebrije.isGood = false
+    } while (answer != "Y" && answer != "y" && answer != "N" &&  answer != "n")
+    // We just want a yes or no, we will check for the user to input that
 
-    if (isTheDogAlive) {
-        /*
-        Logical operators
-        < less than
-        > greater than
-        <= less than or equal to
-        >= greater or equal to
-        == equal to
-        != NOT
-        && AND
-        || OR
-         */
+    println("Mention 2 of its hobbies")
+    // Representation of a for loop in Kotlin
+    for (x in 0..1) {
+        print("Hobby: ")
+        alebrije.hobbies[x] = readLine()
     }
 
-    var myName = "Adrian"
+    println("Mention 2 of its abilities")
+    for (x in 0..1) {
+        print("Ability: ")
+        alebrije.habilities[x] = readLine()
+    }   // You can access to a list values with square brackets like other
+        // programming languages
+    print("\n")
 
-    if (myName == "Nick") {
-        println("Sucker")
-    } else {
-        println("$myName you rock!")
+}
+
+// Function to display our story
+fun displayStory(alebrije: Alebrije) {
+    // Another collection in Kotlin, a map
+    // It contains values that will be use to replace data in the text file
+    val alebrijeMap = mapOf(
+        "_name" to alebrije.name,
+        "_home" to alebrije.home,
+        "_age" to alebrije.age,
+        "_hobby1" to alebrije.hobbies[0],
+        "_hobby2" to alebrije.hobbies[1],
+        "_ability1" to alebrije.habilities[0],
+        "_ability2" to alebrije.habilities[1]
+    )
+
+    // File path saved in these variable
+    val pathText = "src/main/kotlin/alebrije.txt"
+
+    // We pass our path to bring our file in the code
+    val inputStream: InputStream = File(pathText).inputStream()
+    val lineList = mutableListOf<String>()
+
+    // Let's read our text inside the file
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+    lineList.forEach{
+        var result = it
+        // Inside the text there are special characters that will allow us identify those
+        // words we want to replace with our Alebrije data
+        alebrijeMap.forEach { (text, keys) -> result = result.replace(text, keys.toString()) }
+        println("> " + result) // Display our story
     }
 
-    println("\tthis is a tab")
+    // Representation of if statement with an else in Kotlin
+    if (alebrije.isGood) {
+        println("> He loves humans, and whenever people is in \n"
+                + "> trouble he comes to help them.")
+    }
+    else {
+        println("> Despite his magnificence you must be careful, \n"
+                + "> he does not like people very much.")
+    }
+}
 
+fun letsPlay () {
+    var playAgain = "Y"
+
+    do {
+        // We created an alebrije with a type of class Alebrije
+        val alebrije = Alebrije()
+
+        // Let's prompt the user for the Alerbije's data
+        getAlebrijeData(alebrije)
+
+        // Let's show our story!
+        displayStory(alebrije)
+
+        print("\nPress any key to play again or \"Q\" to quit the game: ")
+        playAgain = readLine().toString()
+        print("\n")
+    } while(playAgain != "Q" && playAgain != "q")
+}
+
+// Main function to call our other functions and do the magic
+fun main() {
+    println("\n\t\t\t\tAlebrijes\n" +
+            "In the last century a mexican craftsman came up with\n" +
+            "a revolutionary idea, he started creating alebrijes. \n" +
+            "These are creatures that can be anything you can\n" +
+            "imagine every alebrije created is unique, and you\n" +
+            "will create yoursright now.\n")
+
+    letsPlay()
+
+    println("Thanks for playing! Have a nice day!")
 }
